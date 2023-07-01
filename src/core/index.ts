7d163be692c0687fe6ce258b70f129f434f50ac3
Ali Maktabi <alimaktabi55@yahoo.com>
2023-07-01 17:42:52 +0330

    fixed main algorithm

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,6 +2,7 @@ import { PathNode } from "@/types"
 import { mapInputToNodes } from "@/utils/map-input"
 import PathGreedy from "./path-greedy"
 import ScoreGreedy from "./score-greedy"
+import MainAlgorithm from "./main"
 
 export class Algorithm {
   distancesMatrix: number[][] = []
@@ -25,6 +26,19 @@ export class Algorithm {
   run() {
     const pathGreedyRes = PathGreedy(this)
     const scoreGreedyRes = ScoreGreedy(this)
+
+    const res = MainAlgorithm(
+      this,
+      this.tMax /
+        calculateWeightedAverage(
+          pathGreedyRes.nodesVisitedCount,
+          scoreGreedyRes.nodesVisitedCount,
+          pathGreedyRes.profit,
+          scoreGreedyRes.profit
+        )
+    )
+
+    console.log(res)
   }
 
   protected fillDistancesMatrix() {
@@ -47,4 +61,17 @@ export class Algorithm {
   protected calculateEuclideanDiff(node1: PathNode, node2: PathNode) {
     return Math.sqrt((node1.x - node2.x) ** 2 + (node1.y - node2.y) ** 2)
   }
+}
+
+const calculateWeightedAverage = (
+  value1: number,
+  value2: number,
+  weight1: number,
+  weight2: number
+): number => {
+  const totalWeight = weight1 + weight2
+  const weightedValue1 = value1 * weight1
+  const weightedValue2 = value2 * weight2
+
+  return (weightedValue1 + weightedValue2) / totalWeight
 }
