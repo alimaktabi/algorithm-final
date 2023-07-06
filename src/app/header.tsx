ccac9708ab8e59df60a1c1298ee7cd8a12da8357
Ali Maktabi <alimaktabi55@yahoo.com>
2023-07-06 23:47:36 +0330

    fixed packages, added documentation

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,5 +1,12 @@
+import Link from "next/link"
+
 export default function Header() {
   return (
-    <header className="bg-gray-700 p-5">Orienting Problem Explained</header>
+    <header className="bg-gray-700 flex items-center p-5">
+      <span>Orienting Problem Explained</span>
+      <Link href="/document" className="ml-4">
+        Document
+      </Link>
+    </header>
   )
 }
